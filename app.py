26813ce14e16e98d32115d7a41f9e34205de344b
keasymoneyd <109938732+keasymoneyd@users.noreply.github.com>
2024-03-19 17:26:52 +0800

    app.py

--- app.py
+++ app.py
@@ -36,8 +36,9 @@ def handle_message(event):
     }
     response = openai.ChatCompletion.create(
         messages=[
-            {"role": "user", "content": text1},
-            {"role": "system", "content": "這個GPT的個性資訊:" + str(user_ability)}
+            {"role": "user", "content": text1},    #使用者訊息：這個訊息包含了使用者發送的文字訊息，存儲在 text1 變數中。
+            {"role": "system", "content": "這個GPT的個性資訊:" + str(user_ability)}  #系統回覆：這裡我們將使用者的職業和技能以字典形式存儲在 user_ability 變數中，然後將其轉換為字串。
+
         ],
         model="gpt-3.5-turbo-0125",
         temperature = 0.5,
@@ -45,11 +46,11 @@ def handle_message(event):
     
     try:
         ret = response['choices'][0]['message']['content'].strip()
-        message_counter += 1  
-        print("OpenAI回答次數:", message_counter)  
     except:
         ret = '發生錯誤！'
     line_bot_api.reply_message(event.reply_token,TextSendMessage(text=ret))
+    message_counter += 1  
+    print("OpenAI回答次數:", message_counter)  
 
 if __name__ == '__main__':
     app.run()
